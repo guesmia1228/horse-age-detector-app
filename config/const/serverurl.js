b01d3e8b0736ee42be16591c5e118a367d1851f9
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2019-12-07 20:17:47 -0600

    changed course video url

--- config/const/serverurl.js
+++ config/const/serverurl.js
@@ -3,7 +3,7 @@ export default {
 	basic_url: 'http://agehorseservice.eastus.cloudapp.azure.com:3000/api/',
 	server_url: 'http://agehorseservice.eastus.cloudapp.azure.com:3000',
 	course_video_1: 'https://ml-ref-data.s3.us-east-2.amazonaws.com/course/course1.MOV',
-	course_video_2: 'https://ml-ref-data.s3.us-east-2.amazonaws.com/course/2160p.mov',
+	course_video_2: 'https://ml-ref-data.s3.us-east-2.amazonaws.com/course/course.mov',
 	tutorial_video: 'https://ml-ref-data.s3.us-east-2.amazonaws.com/course/course2.MOV',
 	googlekey: 'AIzaSyDHbEKtybS-hAombXHKcsvpZdDREI0z9cI',
 }
